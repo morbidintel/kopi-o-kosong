0bc92d889cd92adc4196e6a63c359636afa08213
Jeremy Chan <chanjem@gmail.com>
2019-01-26 18:18:08 +0800

    Fix typo

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewMonoBehaviour : MonoBehaviour
+public class InputController : MonoBehaviour
 {
     public GameObject unker;
     public PlayerDrink playerDrink;
