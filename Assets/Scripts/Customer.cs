a23d91f2e636e14e9e5037edb49aa2a5bcaa06a6
Kevin Gomulia <kevin.gomulia@dentsuaegis.com>
2019-01-26 15:50:24 +0800

    Update customer

--- Assets/Scripts/Customer.cs
+++ Assets/Scripts/Customer.cs
@@ -7,5 +7,20 @@ public class Customer : MonoBehaviour
     // Start is called before the first frame update
     public Drink drink;
     public float timeRemaining;
-    
+    public bool success;
+
+    public Customer(Drink desiredDrink, float timeLimit)
+    {
+        this.drink = desiredDrink;
+        this.timeRemaining = timeLimit;
+    }
+
+    public bool SubmitDrink(Drink completedDrink) 
+    {
+        return this.drink.Equals(completedDrink);
+    }
+
+    void Update() {
+        this.timeRemaining -= Time.deltaTime;
+    }
 }
