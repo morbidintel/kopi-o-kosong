863c5e6bfc04421d5c8878eda95a04598c79e0a7
George Chew <hanged94@gmail.com>
2019-01-26 20:54:30 +0800

    Fixed tweens for Unker completion

--- Assets/Scripts/Customer.cs
+++ Assets/Scripts/Customer.cs
@@ -2,53 +2,56 @@
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Customer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public List<Drink> incomplete = new List<Drink>();
-    public List<Drink> fulfilled = new List<Drink>();
-    public float timeRemaining;
-    public bool success;
-    public DOTweenAnimation completeTween;
+	// Start is called before the first frame update
+	public List<Drink> incomplete = new List<Drink>();
+	public List<Drink> fulfilled = new List<Drink>();
+	public float timeRemaining;
+	public bool success;
 
-    public void Init(Difficulty difficulty, float timeLimit)
-    {
-        this.incomplete = difficulty.GenerateDrinkList();
-        this.timeRemaining = timeLimit;
-    }
+	[SerializeField]
+	UnityEvent onComplete;
 
-    public bool SubmitDrink(Drink completedDrink)
-    {
-        foreach (Drink drink in incomplete)
-        {
-            if (drink.Equals(completedDrink))
-            {
-                incomplete.Remove(drink);
-                fulfilled.Add(drink);
-                return true;
-            }
-        }
-        return false;
-    }
+	public void Init(Difficulty difficulty, float timeLimit)
+	{
+		incomplete = difficulty.GenerateDrinkList();
+		timeRemaining = timeLimit;
+	}
 
-    public bool IsCompleted()
-    {
-        return (incomplete.Count == 0);
-    }
+	public bool SubmitDrink(Drink completedDrink)
+	{
+		foreach (Drink drink in incomplete)
+		{
+			if (drink.Equals(completedDrink))
+			{
+				incomplete.Remove(drink);
+				fulfilled.Add(drink);
+				return true;
+			}
+		}
+		return false;
+	}
 
-    public void OnComplete()
-    {
-        completeTween.DOPlay();
-    }
+	public bool IsCompleted()
+	{
+		return (incomplete.Count == 0);
+	}
 
-    void Update()
-    {
-        this.timeRemaining -= Time.deltaTime;
-    }
+	public void OnComplete()
+	{
+		onComplete.Invoke();
+	}
 
-    public void OnFinishTween()
-    {
-        Destroy(gameObject);
-    }
+	void Update()
+	{
+		timeRemaining -= Time.deltaTime;
+	}
+
+	public void OnFinishTween()
+	{
+		Destroy(gameObject);
+	}
 }
--- Assets/Scripts/Orderer.cs
+++ Assets/Scripts/Orderer.cs
@@ -1,95 +1,96 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using Gamelogic.Extensions;
+using UnityEngine;
 
 public class Orderer : Singleton<Orderer>
 {
-    private List<Customer> orders = new List<Customer>();
-    private Auntie auntie;
+	private List<Customer> orders = new List<Customer>();
+	private Auntie auntie;
 
-    public GameObject customerPrefab;
+	public GameObject customerPrefab;
 
-    private Difficulty difficulty;
+	private Difficulty difficulty;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        difficulty = GameController.Instance.difficulty;
-        StartCoroutine(AuntieCoroutine());
-        StartCoroutine(OrderCoroutine());
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		difficulty = GameController.Instance.difficulty;
+		StartCoroutine(AuntieCoroutine());
+		StartCoroutine(OrderCoroutine());
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        foreach(Customer customer in orders)
-        {
-            if (customer.timeRemaining < 0.0f)
-            {
-                orders.Remove(customer);
-                //@todo: penalty
-            }
-        }
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		foreach (Customer customer in orders)
+		{
+			if (customer.timeRemaining < 0.0f)
+			{
+				orders.Remove(customer);
+				//@todo: penalty
+			}
+		}
+	}
 
-    IEnumerator AuntieCoroutine()
-    {
-        while(true)
-        {
-            GenerateAuntie(difficulty);
-            yield return new WaitForSeconds(5);
-        }
-    }
+	IEnumerator AuntieCoroutine()
+	{
+		while (true)
+		{
+			GenerateAuntie(difficulty);
+			yield return new WaitForSeconds(5);
+		}
+	}
 
-    IEnumerator OrderCoroutine()
-    {
-        while(true)
-        {
-            GenerateOrder(difficulty);
-            yield return new WaitForSeconds(10);
-        }
-    }
+	IEnumerator OrderCoroutine()
+	{
+		while (true)
+		{
+			GenerateOrder(difficulty);
+			yield return new WaitForSeconds(10);
+		}
+	}
 
-    public void checkAndScoreDrink(Drink drink)
-    {
-        if (auntie != null)
-        {
-            if (auntie.SubmitDrink(drink))
-            {
-                // SCORE!
-                auntie = null;
-            }
-            return;
-        }
+	public void checkAndScoreDrink(Drink drink)
+	{
+		if (auntie != null)
+		{
+			if (auntie.SubmitDrink(drink))
+			{
+				// SCORE!
+				auntie = null;
+			}
+			return;
+		}
 
-        foreach (Customer customer in orders)
-        {
-            if (customer.SubmitDrink(drink))
-            {
-                //check if completed and score
-                if (customer.IsCompleted())
-                {
-                    //SCORE!
-                    customer.OnComplete();                }
+		foreach (Customer customer in orders)
+		{
+			if (customer.SubmitDrink(drink))
+			{
+				//check if completed and score
+				if (customer.IsCompleted())
+				{
+					//SCORE!
+					customer.OnComplete();
+				}
 
-                return;
-            }
-        }
-        //todo: penalty
-    }
+				return;
+			}
+		}
+		//todo: penalty
+	}
 
-    void GenerateOrder(Difficulty stageDifficulty)
-    {
-        Customer cust = Instantiate(customerPrefab).GetComponent<Customer>();
-        cust.Init(difficulty, 60.0f);
-        orders.Add(cust);
-        //orders.Add(new Customer(stageDifficulty, 60.0f));
-    }
+	void GenerateOrder(Difficulty stageDifficulty)
+	{
+		Customer cust = Instantiate(customerPrefab, transform).GetComponent<Customer>();
+		cust.Init(difficulty, 60.0f);
+		orders.Add(cust);
+		//orders.Add(new Customer(stageDifficulty, 60.0f));
+	}
 
-    void GenerateAuntie(Difficulty stageDifficulty)
-    {
-        var threshold = 0.15f * stageDifficulty.stageDifficulty - 0.15f;
-        if (Random.Range(0f, 1f) < threshold) auntie = new Auntie(stageDifficulty, 10.0f);
-    }
+	void GenerateAuntie(Difficulty stageDifficulty)
+	{
+		var threshold = 0.15f * stageDifficulty.stageDifficulty - 0.15f;
+		if (Random.Range(0f, 1f) < threshold) auntie = new Auntie(stageDifficulty, 10.0f);
+	}
 }
