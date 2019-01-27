c2e71a1673cf0f003293ed3925b6627048b90010
Kevin Gomulia <kevin.gomulia@dentsuaegis.com>
2019-01-27 11:59:36 +0800

    Added buttons below the ingredients that will disappear after 10 seconds... Fast&Furious

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,68 +7,105 @@ using UnityEngine.UI;
 
 public class GameController : Singleton<GameController>
 {
-	public const float maxTimeRemaining = 60f;
-	public float timeRemaining;
-	public float timeIncrement;
-	public float restartDelay = 4f;
-	public GameObject gameOverPanel;
-	public Text scoreText;
-	public int score;
-	private int oldScore;
-	bool gameHasEnded;
-	public Difficulty difficulty = new Difficulty(1);
+    public const float maxTimeRemaining = 60f;
+    public float timeRemaining;
+    public float timeIncrement;
+    public float restartDelay = 4f;
+    public GameObject gameOverPanel;
+    public Text scoreText;
+    public int score;
+    private int oldScore;
+    bool gameHasEnded;
+    public Difficulty difficulty = new Difficulty(1);
 
-	void Start()
-	{
-		timeRemaining = 60;
-	}
+    void Start()
+    {
+        timeRemaining = 60;
+        StartCoroutine(InitiateUIButtons());
+    }
 
-	void Update()
-	{
-		UpdateScore();
-		timeRemaining -= Time.deltaTime;
-		if (timeRemaining < 0)
-		{
-			GameOver();
-		}
+    IEnumerator InitiateUIButtons()
+    {
+        GameObject qButton = GameObject.Find("Q");
+        GameObject wButton = GameObject.Find("W");
+        GameObject eButton = GameObject.Find("E");
+        GameObject rButton = GameObject.Find("R");
+        GameObject tButton = GameObject.Find("T");
+        GameObject yButton = GameObject.Find("Y");
+
+        ConfigureText(qButton, "Q");
+        ConfigureText(wButton, "W");
+        ConfigureText(eButton, "E");
+        ConfigureText(rButton, "R");
+        ConfigureText(tButton, "T");
+        ConfigureText(yButton, "Y");
+        yield return new WaitForSeconds(10);
+
+        HideButton(qButton);
+        HideButton(wButton);
+        HideButton(eButton);
+        HideButton(rButton);
+        HideButton(tButton);
+        HideButton(yButton);
+    }
+
+    void HideButton(GameObject button)
+    {
+        button.SetActive(false);
+    }
+    void ConfigureText(GameObject buttonText, string newText)
+    {
+        buttonText.GetComponentInChildren<Text>().fontSize = 40;
+        buttonText.GetComponentInChildren<Text>().text = newText;
+        buttonText.GetComponentInChildren<Text>().color = Color.white;
+    }
+
+    void Update()
+    {
+        UpdateScore();
+        timeRemaining -= Time.deltaTime;
+        if (timeRemaining < 0)
+        {
+            GameOver();
+        }
         difficulty = Difficulty.GetDifficulty(score);
-	}
+    }
 
-	void GameOver()
-	{
-		if (!gameHasEnded)
-		{
-			gameHasEnded = true;
-			Debug.Log("Game Over");
-			Invoke("Restart", restartDelay);
-			UpdateScore();
-			gameOverPanel.SetActive(true);
+    void GameOver()
+    {
+        if (!gameHasEnded)
+        {
+            gameHasEnded = true;
+            Debug.Log("Game Over");
+            Invoke("Restart", restartDelay);
+            UpdateScore();
+            gameOverPanel.SetActive(true);
 
-		}
+        }
 
-	}
+    }
 
-	public void Restart()
-	{
-		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-	}
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 
-	public void AddScore(int value)
-	{
-		score += value;
-	}
+    public void AddScore(int value)
+    {
+        score += value;
+    }
 
-	void UpdateScore()
-	{
-		if (score != oldScore)
-		{
-			oldScore++;
-			scoreText.GetComponent<Text>().text = oldScore.ToString();
-		}
-	}
+    void UpdateScore()
+    {
+        if (score != oldScore)
+        {
+            oldScore++;
+            scoreText.GetComponent<Text>().text = oldScore.ToString();
+        }
+    }
 
-	public void AddTime()
-	{
-		timeRemaining = Mathf.Min(timeRemaining += timeIncrement, maxTimeRemaining);
-	}
+    public void AddTime()
+    {
+        timeRemaining = Mathf.Min(timeRemaining += timeIncrement, maxTimeRemaining);
+    }
 }
